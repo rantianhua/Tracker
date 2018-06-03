1ffcba75de4687be66c4728981c65f4e10d902d7
Tianhua Ran <tianhua.ran@liulishuo.com>
2018-06-03 22:55:12 +0800

    feat: 添加 pipeline ，保存数据到 json 文件

--- Tracker/pipelines.py
+++ Tracker/pipelines.py
@@ -5,7 +5,21 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from scrapy.exporters import JsonLinesItemExporter
+
 
 class TrackerPipeline(object):
+
+    def __init__(self):
+        self.file = open('items.json', 'wb')
+        self.exporter = JsonLinesItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
+        self.exporter.start_exporting()
+
+
     def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
+
+
+    def close_spider(self, spider):
+        self.file.close()
